59a5bf252e7207cfa772bc5ce8b26e032ff85826
amirahad <arahad4783@gmail.com>
2022-04-26 03:39:37 +0600

    context added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
 import "./navbar.css";
 import SearchBar from "./SearchBar";
+import { ThemeContext } from "../context/ThemeContext";
 
 export default function Navbar() {
+  const { color } = useContext(ThemeContext)
   return (
-    <div className="navbar">
+    <div className="navbar" style={{background: color}}>
       <nav>
         <Link to="/" className="brand">
           <h1>RECIPES</h1>
